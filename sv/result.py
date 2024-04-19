ae498a1add40227e3ccfbb00b82c1ae52d89069d
sulin2010 <jean_lin2010@163.com>
2024-04-19 15:54:18 +0800

    2024.4.19

--- sv/result.py
+++ sv/result.py
@@ -4,7 +4,7 @@ import argparse
 import sys
 import os
 import re
-#bindir = os.path.abspath(os.path.dirname(__file__))
+bindir = os.path.abspath(os.path.dirname(__file__))
 import time
 from datetime import datetime
 import HTSeq
@@ -21,7 +21,10 @@ Project :
 Description : 
 Version : 
 Last Modification : 
-Changes : 
+2024-4-19: SV过滤规则：
+	1、断点在基因区间：SV两侧断点基因属于以下几种情况则进行去除：旁系同源基因、相同基因，相同基因家族。
+	2、任一断点在基因间区：断点区间未覆盖任何基因的SV则进行去除
+	3、保留任何可能致病性的SV结果
 \033[0m'''
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(filename)s[line:%(lineno)d ] %(levelname)-4s : %(message)s',datefmt='%a, %d %b %Y %H:%M:%S',)
@@ -106,7 +109,7 @@ def AnnoFeature(gene1,gene_tran_dic):
 			if len(g1_anno) > 1 :
 				print('B',gene1_list)
 			elif len(g1_anno) == 0:
-				flag1=''
+				flag1='intergenic'
 				anno1 = ['-','-','intergenic','']	
 			else:
 				anno1 = g1_anno[0].split(':')
@@ -135,28 +138,48 @@ def GetSupport(INFO,detail):
 	dep = str(pr_ref+sr_ref+pr_alt+sr_alt)
 	return(vaf,pr,sr,dep)
 
+def ParalogGene(paralog_file):
+	para_in = open(paralog_file)
+	para_dic = {}
+	for index,line in enumerate(para_in):
+		lines = line.rstrip('\n').split('\t')
+		for gg in lines:
+			para_dic[gg] = str(index)
+	para_in.close()
+	return(para_dic)
+
 def main():
 	parser=argparse.ArgumentParser(description=__doc__,
 			formatter_class=argparse.RawDescriptionHelpFormatter,
 			epilog='author:\t{0}\nmail:\t{1}'.format(__author__,__mail__))
 	parser.add_argument('-ref','--refgene',help='input refgene file',default='/data/software/app/AnnotSV-2.3/share/AnnotSV/Annotations_Human/RefGene/GRCh37/refGene.sorted.bed')
+	parser.add_argument('-p','--paralog',help='input Paralogue list file',default=bindir+'/data/FusionGenePairParalogueList20131217.txt')
+	parser.add_argument('-gf','--genefamily',help='input gene family list file',default=bindir+'/data/FusionGeneFamilyList20131217.txt')
 	parser.add_argument('-i','--input',help='input chr:breakpoint file',required=True)
 	parser.add_argument('-s','--sample',help='input sample name ',required=True)
 	parser.add_argument('-o','--output',help='output file',required=True)
+	parser.add_argument('-f','--filter',help='output filter file',required=True)
 	args=parser.parse_args()
 	
 	#
 	refgene = args.refgene
+	paralog_file = args.paralog
+	genefamily_file = args.genefamily
 	input = args.input
 	output = args.output
 	sample = args.sample
+	filterout = args.filter
 	#
 	logging.info(refgene)
+	logging.info(paralog_file)
+	logging.info(genefamily_file)
 	new_genes = ReadRefGene(refgene)
+	paralog_dic = ParalogGene(paralog_file)
+	genefamily_dic = ParalogGene(genefamily_file)
 	gene_tran_dic = GeneTran(input)
 	filein = open(input)
 	fileout = open(output,'w')
-	fileout.write('chr1\tPosition1\tGene1\tTranscript1\tFeature1\tStrand1\tchr2\tPosition2\tGene2\tTranscript2\tFeature2\tStrand2\tFeatureType\tSVType\tVAF\tSpanning_paired-read\tSplit_reads\tdepth\tweight\tdecipher\tDGV\tclinvar\tPred\t{ss}\n'.format(ss=sample))
+	fileout.write('chr1\tPosition1\tGene1\tTranscript1\tFeature1\tStrand1\tchr2\tPosition2\tGene2\tTranscript2\tFeature2\tStrand2\tFeatureType\tSVType\tVAF\tSpanning_paired-read\tSplit_reads\tdepth\tCoveredGenes\tweight\tdecipher\tDGV\tclinvar\tPred\t{ss}\n'.format(ss=sample))
 	for index,line in enumerate(filein):
 		lines = line.rstrip('\n').split('\t')
 		if index == 0:
@@ -174,11 +197,29 @@ def main():
 		anno2,flag2 = AnnoFeature(gene2,gene_tran_dic)
 		vaf,pr,sr,dep = GetSupport(info['FORMAT'],info[sample])
 		flag = flag1+'-'+flag2
-		if anno1[0] == anno2[0]:
-			flag = 'same_gene'
-		result = [chrom,pos1]+anno1+[chrom,pos2]+anno2+[flag,info['SV type'],vaf,pr,sr,dep,'-','-',info['DGV_LOSS_IDs'],'-',info['dbVar_status'],sample]
+		gg1=anno1[0]
+		gg2=anno2[0]
+		if gg1 != '-' and gg2 != '-':
+			if gg1 == gg2:
+				flag = 'same_gene'
+			if  gg1 in paralog_dic and gg2 in paralog_dic and  paralog_dic[anno1[0]] == paralog_dic[anno2[0]]:
+				flag = flag+',paralog_gene'
+			if  gg1 in genefamily_dic and gg2 in genefamily_dic and  genefamily_dic[anno1[0]] == genefamily_dic[anno2[0]]:
+				flag = flag+',same_genefamily'
+		result = [chrom,pos1]+anno1+[chrom,pos2]+anno2+[flag,info['SV type'],vaf,pr,sr,dep,info['Gene name'],'-','-',info['DGV_LOSS_IDs'],'-',info['dbVar_status'],sample]
 		result = map(str,result)
 		fileout.write('\t'.join(result)+'\n')
+	dt = pd.read_csv(output,sep='\t')
+	index = dt['FeatureType'].str.contains('same_gene') | dt['FeatureType'].str.contains('paralog_gene') | dt['FeatureType'].str.contains('same_genefamily')
+	dt['Filter'] = 'Pass'
+	dt.loc[index,'Filter'] = 'NoPass'
+	index = dt['FeatureType'].str.contains('intergenic') & dt['CoveredGenes'].isna() 
+	dt.loc[index,'Filter'] = 'NoPass'
+	index = ~dt['Pred'].isna()
+	dt.loc[index,'Filter'] = 'Pass'
+	index = dt['Filter'] == 'Pass'
+	dt = dt.loc[index]
+	dt.to_csv(filterout,index=False,header=True,sep='\t')
 	logging.info('Finish!')
 		
 
